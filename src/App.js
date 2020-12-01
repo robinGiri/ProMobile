c67c248ba3bb54124b4ae2e0e694b4500fda1957
robinGiri <girirobin2018@gmail.com>
2020-12-01 17:46:38 +0600

    react-router-dom set for routing throuth pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {
+  Switch,
+  Route,
+} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar'
+import Cart from './components/Cart'
+import Default from './components/Default'
+import Details from './components/Details'
+import ProductList from './components/ProductList'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={ProductList} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/details" component={Details} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <h2>hay from nav</h2>
+            </div>
+        )
+    }
+}
