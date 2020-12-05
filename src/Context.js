6f71daf82a26f3a7160ea569eeb1555bead44154
robinGiri <girirobin2018@gmail.com>
2020-12-05 21:37:02 +0545

    changed data fetching methode and life cycle methode introduced

--- src/Context.js
+++ src/Context.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react'
-import {storeProducts , detailProduct} from './data'
+import { storeProducts, detailProduct } from './data'
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        product: storeProducts,
+        product: [],
         drtail: detailProduct
+    }
+    componentDidMount() {
+        this.setProduct();
+    }
+
+    setProduct = () => {
+        let tempProducts = [];
+        storeProducts.forEach(items => {
+            const singleItme = { ...items }
+            tempProducts = [...tempProducts, singleItme];
+        });
+        this.setState(() => {
+            return { product: tempProducts }
+        })
+
     }
     handelDetail = () => {
         console.log('hay from detail')
     }
-    addToCart = () =>{
+    addToCart = () => {
         console.log('lala cart')
     }
     render() {
         return (
-            <ProductContext.Provider value={{...this.state, handelDetail: this.handelDetail, addToCart: this.addToCart}}>
-               {this.props.children} 
+            <ProductContext.Provider value={{ ...this.state, handelDetail: this.handelDetail, addToCart: this.addToCart }}>
+                {this.props.children}
             </ProductContext.Provider>
         )
     }
@@ -25,4 +40,4 @@ class ProductProvider extends Component {
 
 const ProductConsumer = ProductContext.Consumer;
 
-export  { ProductProvider, ProductConsumer}
+export { ProductProvider, ProductConsumer }
